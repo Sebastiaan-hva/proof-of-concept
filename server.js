01675b967e8007cdbe43a85db8392ce4d36d8169
SebastiaanHag <sebastiaan.hagoort@hva.nl>
2025-06-05 20:24:04 +0200

    #7 data vorig jaar directus ingeladen
    getest

--- server.js
+++ server.js
@@ -13,10 +13,19 @@ app.engine('liquid', engine.express())
 app.set('views', './views')
 
 
-app.get('/', async function (request, response) {
+app.get('/house/:id', async function (request, response) {
 
-   response.render('index.liquid');
-})
+  // directus data van vorig jaar van Krijn
+  const housesURL = 'https://fdnd-agency.directus.app/items/f_houses';
+  const housesResponse = await fetch(housesURL);
+  const housesJSON = await housesResponse.json();
+
+  const house = housesJSON.data[0];
+  // lokale json als ik die nog wil gaan gebruiken
+  // const data = require('/listing.json');
+   response.render('index.liquid', { housesResponse: house });
+});
+
 
 app.set('port', process.env.PORT || 8000)
 
